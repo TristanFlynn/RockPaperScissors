9e9826e5e04e8b58ee23d5c5d504550194ef6bf6
TristanFlynn <TristanFlynnt@gmail.com>
2023-06-20 16:07:13 -0400

    playGame function is complete. Results complete with playerMove parameter inside function.

--- java.js
+++ java.js
@@ -1,3 +1,46 @@
+function playGame(playerMove) {
+// This function will operate when a "playerMove" button is clicked
+//Results are compared to "computerMove" which operates the computerChoice function
+  const computerMove = getComputerChoice();
+
+  let result = '';
+
+  if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie!';
+    } else if (computerMove === 'paper') {
+      result = 'You Lose!';
+    } else if (computerMove === 'scissors') {
+      result = 'You Win!';
+    }
+  }
+
+  if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You Win!';
+    } else if (computerMove === 'paper') {
+      result = 'Tie!';
+    } else if (computerMove === 'scissors') {
+      result = 'You Lose!';
+    }
+  }
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You Lose!';
+    } else if (computerMove === 'paper') {
+      result = 'You Win!';
+    } else if (computerMove === 'scissors') {
+      result = 'Tie!';
+    }
+  }
+
+  console.log(`you picked ${playerMove}, computer picked ${computerMove}, by default, ${result}`);
+}
+
+
+
+
 function getComputerChoice () {
 // For Computer's move, I am starting a function that creates a random number
   const randomNumber = Math.random();
@@ -13,4 +56,3 @@ function getComputerChoice () {
 // By generating a random number, each random event i split into equal thirds for possibilties
   return computerMove;
 }
-console.log(getComputerChoice());
